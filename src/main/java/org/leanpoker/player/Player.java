b8841808859592ceeffd34338c0a63cb546e2248
Kostja <Kostja@DESKTOP-81Q2584>
2017-02-28 10:52:11 +0100

    Sysout

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,9 +9,12 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+    	System.err.println("betRequest: " +  request.toString());
         return 200;
     }
 
     public static void showdown(JsonElement game) {
+
+    	System.err.println("showdown: " +  game.toString());
     }
 }
